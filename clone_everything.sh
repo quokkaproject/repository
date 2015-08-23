9bf27771e313f51edd1220916fbf21f3588c67d1
Bruno Rocha <rochacbruno@gmail.com>
2015-08-23 19:03:57 -0300

    Create clone_everything.sh

---
+++ clone_everything.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+git clone https://github.com/quokkaproject/quokka
+git clone https://github.com/quokkaproject/flask-admin quokka-flask-admin
+git clone https://github.com/dimka665/awesome-slugify quokka-flask-security
+git clone https://github.com/quokkaproject/speaklater quokka-speaklater
+git clone https://github.com/quokkaproject/flask-mongoengine quokka-flask-mongoengine
+git clone https://github.com/quokkaproject/quokka-themes
+git clone https://github.com/quokkaproject/flask-htmlbuilder quokka-flask-htmlbuilder
+git clone https://github.com/quokkaproject/twill quokka-twill
+
+git clone https://github.com/mitsuhiko/flask
+git clone https://github.com/mitsuhiko/jinja2
+git clone https://github.com/mitsuhiko/click
+git clone https://github.com/mitsuhiko/werkzeug
+git clone https://github.com/mitsuhiko/babel
+git clone https://github.com/mitsuhiko/markupsafe
+git clone https://github.com/mitsuhiko/itsdangerous
+
+git clone https://github.com/maxcountryman/flask-login/
+git clone https://github.com/mattupstate/flask-mail/
+git clone https://github.com/mattupstate/flask-principal/
+
+
+git clone https://github.com/MongoEngine/mongoengine
+git clone https://github.com/dimka665/awesome-slugify
+git clone https://github.com/jek/blinker
+git clone https://github.com/mrjoes/flask-babelex
+git clone https://github.com/thadeusb/flask-cache/
+git clone https://github.com/zzzsochi/Flask-Gravatar/
+git clone https://github.com/ellisonleao/flask-mistune
+git clone https://github.com/lepture/mistune
+git clone https://github.com/lepture/flask-oauthlib
+git clone https://github.com/idan/oauthlib
+git clone https://github.com/requests/requests-oauthlib
+git clone https://github.com/python-pillow/Pillow
+git clone https://github.com/kennethreitz/requests
+git clone https://github.com/ellisonleao/pyshorteners
+git clone https://github.com/pydanny/cached-property
+git clone https://github.com/rochacbruno/shiftpy
+git clone https://github.com/mgood/flask-debugtoolbar
+git clone https://github.com/mongodb/mongo-python-driver
+git clone https://github.com/lepture/flask-wtf
+git clone https://github.com/jarus/flask-testing/
+
+wget https://pypi.python.org/packages/source/P/PyRSS2Gen/PyRSS2Gen-1.1.tar.gz
